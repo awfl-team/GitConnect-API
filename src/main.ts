aa08f25ceba2a3a1bf3230f05c9d965fdb2afd6d
lbourgeois <lucasbourgeois60@hotmail.fr>
2020-02-16 14:47:37 +0100

    add cors header

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { ValidationPipe } from '@nestjs/common'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create<NestExpressApplication>(AppModule)
+    const corsOption: CorsOptions = { origin: '*' }
     const options = new DocumentBuilder()
         .setTitle('GitConnect API')
         .setDescription('available method in GitConnect API')
@@ -13,8 +15,10 @@ async function bootstrap(): Promise<void> {
         .addBearerAuth()
         .build()
     const document = SwaggerModule.createDocument(app, options)
+
     SwaggerModule.setup('api', app, document)
     app.useGlobalPipes(new ValidationPipe())
+    app.enableCors(corsOption)
     await app.listen(3000)
 }
 bootstrap()
